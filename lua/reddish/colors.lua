cd2835d627852f38df9b65a3fb2dd948f602b52c
Seger Sars <segersars@gmail.com>
2024-01-23 14:00:00 +0100

    small change

--- lua/reddish/colors.lua
+++ lua/reddish/colors.lua
@@ -1,14 +1,14 @@
 -- color palette
 local c = {
-    bright3      = '#f6c2a5',
-    bright2      = '#ea9a7d',
-    bright1      = '#e16a5e',
-    normal       = '#d22f32',
-    dark1        = '#b1274e',
-    dark2        = '#931b46',
-    dark3        = '#631033',
-    dark4        = '#42001f',
-    background   = '#101010',
+    bright3    = '#f6c2a5',
+    bright2    = '#ea9a7d',
+    bright1    = '#e16a5e',
+    normal     = '#d22f32',
+    dark1      = '#b1274e',
+    dark2      = '#931b46',
+    dark3      = '#631033',
+    dark4      = '#42001f',
+    background = '#101010',
 }
 
 local highlights = {
@@ -30,12 +30,12 @@ local highlights = {
     Search      = { fg = c.dark2, bg = c.bright3 },
     Substitute  = { fg = c.dark2, bg = c.bright3 },
     Pmenu       = { fg = c.bright3, bg = c.dark3 },
-    PmenuSel       = { fg = c.dark4, bg = c.bright3 },
+    PmenuSel    = { fg = c.dark4, bg = c.bright3 },
     Whitespace  = { fg = c.bright2, bg = c.background },
 
     Comment     = { fg = c.bright2, bg = c.background },
-    String      = { fg = c.bright2, bg = c.background },
-    Character   = { fg = c.bright2, bg = c.background },
+    String      = { fg = c.bright1, bg = c.background },
+    Character   = { fg = c.bright1, bg = c.background },
     Number      = { fg = c.bright3, bg = c.background },
     Float       = { fg = c.bright3, bg = c.background },
     Boolean     = { fg = c.normal, bg = c.background },
@@ -44,7 +44,7 @@ local highlights = {
     Function    = { fg = c.bright2, bg = c.background },
 
     Statement   = { fg = c.normal, bg = c.background },
-    Operator   = { fg = c.bright2, bg = c.background },
+    Operator    = { fg = c.bright2, bg = c.background },
 
     PreProc     = { fg = c.normal, bg = c.background },
 
